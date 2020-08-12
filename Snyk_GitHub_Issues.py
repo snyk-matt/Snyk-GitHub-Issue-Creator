031c6aa8e82393626ad4aeb158a1a1a771df1a36
Matt @ Snyk <59509697+snyk-matt@users.noreply.github.com>
2020-08-12 12:17:54 -0400

    removing a couple of workflows

--- Snyk_GitHub_Issues.py
+++ Snyk_GitHub_Issues.py
@@ -66,68 +66,72 @@ results_output = results.json()
 # need to do this (for now) in order to grab the unique Snyk issue ID
 open_issues = repo.get_issues(state='open')
 for issue in open_issues:
-  issue_object_to_string = str(issue)
-  issue_split = issue_object_to_string[13:]
-  issue_list = issue_split.split('"')[0]
-  current_gh_issues.append(issue_list)
-  split_current_issues = [i.split('- ')[1] for i in current_gh_issues]
-  
+    issue_object_to_string = str(issue)
+    issue_split = issue_object_to_string[13:]
+    issue_list = issue_split.split('"')[0]
+    current_gh_issues.append(issue_list)
+    split_current_issues = [i.split('- ')[1] for i in current_gh_issues]
 
 for issue in results_output['results']:
-  total_snyk_issues = total_snyk_issues + 1
+    total_snyk_issues = total_snyk_issues + 1
 
 if total_snyk_issues > 0:
-  print(f"Total Snyk issues found: {total_snyk_issues}")
+    print(f"Total Snyk issues found: {total_snyk_issues}")
 
 for issue in results_output['results']:
-  issue_title = issue['issue']['title']
-  issue_type = issue['issue']['type']
-  issue_id = issue['issue']['id']
-  issue_url = issue['issue']['url']
-  issue_severity = issue['issue']['severity']
-  issue_version = issue['issue']['version']
-  issue_introducedDate = issue['introducedDate']
-# if the issue id from the snyk API is not in the list of issues we pulled from GH
-# add additional meta data to the issue, then create the issue
-# using Snyk's issue ID as it's a unique identifier
-# using Snyk's issue ID will prevent duplicated from being entered
-  if issue_id not in split_current_issues:
-    project_name = issue['project']['name'] 
-    project_url = issue['project']['url']
-    project_targetFile = issue['project']['targetFile']
-    new_issues = new_issues + 1
-    
-    repo.create_issue(title=issue_title + " | Snyk ID - " + issue_id, body=("Title: " + issue_title) + "\n" 
-                                                                      + ("  Snyk ID: " + issue_id) + "\n"
-                                                                      + ("  URL: " + issue_url) + "\n"
-                                                                      + ("  Severity: " + issue_severity) + "\n"
-                                                                      + ("  Version: " + issue_version) + "\n"
-                                                                      + ("  Introduced Date: " + issue_introducedDate) + "\n"
-                                                                      + ("  Projects with Vulnerability: " + project_name) + "\n"
-                                                                      + ("  Project URL: " + project_url) + "\n"
-                                                                      + ("  Target File: " + project_targetFile)
-                      )
+    issue_title = issue['issue']['title']
+    issue_type = issue['issue']['type']
+    issue_id = issue['issue']['id']
+    issue_url = issue['issue']['url']
+    issue_severity = issue['issue']['severity']
+    issue_version = issue['issue']['version']
+    issue_introducedDate = issue['introducedDate']
+    # if the issue id from the snyk API is not in the list of issues we pulled from GH
+    # add additional meta data to the issue, then create the issue
+    # using Snyk's issue ID as it's a unique identifier
+    # using Snyk's issue ID will prevent duplicated from being entered
+    if issue_id not in split_current_issues:
+        project_name = issue['project']['name']
+        project_url = issue['project']['url']
+        project_targetFile = issue['project']['targetFile']
+        new_issues = new_issues + 1
 
-#this section closes github issues once the vulns have been fixed in Snyk:
+        repo.create_issue(title=issue_title + " | Snyk ID - " + issue_id, body=("Title: " + issue_title) + "\n"
+                                                                               + ("  Snyk ID: " + issue_id) + "\n"
+                                                                               + ("  URL: " + issue_url) + "\n"
+                                                                               + (
+                                                                                       "  Severity: " + issue_severity) + "\n"
+                                                                               + ("  Version: " + issue_version) + "\n"
+                                                                               + (
+                                                                                       "  Introduced Date: " + issue_introducedDate) + "\n"
+                                                                               + (
+                                                                                       "  Projects with Vulnerability: " + project_name) + "\n"
+                                                                               + (
+                                                                                       "  Project URL: " + project_url) + "\n"
+                                                                               + (
+                                                                                       "  Target File: " + project_targetFile)
+                          )
+
+# this section closes github issues once the vulns have been fixed in Snyk:
 for issue_from_snyk in results_output['results']:
-  snyk_issue_title = issue_from_snyk['issue']['title']
-  snyk_issue_type = issue_from_snyk['issue']['type']
-  snyk_issue_id = issue_from_snyk['issue']['id']
-  current_snyk_issues.append(snyk_issue_title + " | Snyk ID - " + snyk_issue_id)
+    snyk_issue_title = issue_from_snyk['issue']['title']
+    snyk_issue_type = issue_from_snyk['issue']['type']
+    snyk_issue_id = issue_from_snyk['issue']['id']
+    current_snyk_issues.append(snyk_issue_title + " | Snyk ID - " + snyk_issue_id)
 
 for gh_open_issue in open_issues:
-  open_gh_issues.append(gh_open_issue.title)
-  if gh_open_issue.title not in current_snyk_issues:
-    print(gh_open_issue.title + " has been fixed in Snyk. The GitHub issue will be closed...")
-    gh_open_issue.edit(state='closed')
+    open_gh_issues.append(gh_open_issue.title)
+    if gh_open_issue.title not in current_snyk_issues:
+        print(gh_open_issue.title + " has been fixed in Snyk. The GitHub issue will be closed...")
+        gh_open_issue.edit(state='closed')
 
-#output whether or not there are any new issues found, and how many
+# output whether or not there are any new issues found, and how many
 if new_issues != 0:
-  if new_issues > 1:
-    print(f"{new_issues} new issues found!")
-    print(f"Added {new_issues} issues to GitHub Issues")
-  else:
-    print(f"{new_issues} new issue found!")
-    print(f"Added {new_issues} issue to GitHub Issues")
+    if new_issues > 1:
+        print(f"{new_issues} new issues found!")
+        print(f"Added {new_issues} issues to GitHub Issues")
+    else:
+        print(f"{new_issues} new issue found!")
+        print(f"Added {new_issues} issue to GitHub Issues")
 else:
-  print("No new issues found since last scan.")
+    print("No new issues found since last scan.")
